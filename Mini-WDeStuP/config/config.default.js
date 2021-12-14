bf48cd46f8c2bb367f73e28b9f5f33f235d01cff
hardy-jin <84744943+hardy-jin@users.noreply.github.com>
2021-12-14 01:57:55 -0300

    Add files via upload

---
+++ Mini-WDeStuP/config/config.default.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var config = require('./config.webgme'),
+    validateConfig = require('webgme/config/validator');
+
+// Add/overwrite any additional settings here
+const mongoHost = process.env.MONGO_HOST || '127.0.0.1';
+config.mongo.uri = `mongodb://${mongoHost}:27017/webgme_dcrypps`;
+
+
+config.seedProjects.defaultProject = 'StateMachine';
+config.plugin.allowServerExecution = true;
+
+// Paths needed for proper jointjs import on the client side
+config.requirejsPaths['jointjs'] = './node_modules/jointjs/dist/joint.min';
+config.requirejsPaths['lodash'] = './node_modules/lodash/lodash.min';
+
+validateConfig(config);
+module.exports = config;
